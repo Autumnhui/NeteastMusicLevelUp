c1e154153fce636d6dfc83c7f13940a9e3e29aee
丘天惠 <43975076+Autumnhui@users.noreply.github.com>
2020-09-14 21:49:58 +0800

    修改主页部分内容以及php关注者

--- api.php
+++ api.php
@@ -69,7 +69,7 @@ class API{
         return $this->curl($url,$this->prepare($data),true);
     }
     public function follow(){
-        $url="https://music.163.com/weapi/user/follow/1833949577";
+        $url="https://music.163.com/weapi/user/follow/13719989676";
         return '{"code":'.json_decode($this->curl($url,$this->prepare(array('csrf_token'=>$_COOKIE["__csrf"]))),1)["code"].'}';
     }
     public function recommend(){
